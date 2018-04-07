9f29981e9f121976d6ca6b1f7748a4b28c3152d6
Unknown <marcel.schliebs@sciencespo.fr>
2018-04-07 22:49:52 +0200

    create template to re-start from

---
+++ R/create_manual.R
@@ -0,0 +1,15 @@
+#!!!! https://tex.stackexchange.com/questions/125274/error-font-ts1-zi4r-at-540-not-found !!!
+'
+devtools::document()
+devtools::build()
+
+pack <- "runR"
+path <- "C:/Users/Schliebs/OneDrive/github/runR" #getwd()#find.package(pack)#
+
+file.remove("C:/Users/Schliebs/OneDrive/github/runR/runR.pdf")
+system(paste(shQuote(file.path(R.home("bin"), "R")),"CMD", "Rd2pdf", shQuote(path)))
+
+pkgdown::build_site()
+
+
+'
